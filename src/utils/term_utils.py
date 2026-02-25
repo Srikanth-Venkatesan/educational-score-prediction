dd3cb7762cc40bc74c0f1eb2c23a76edf15f3432
Srikanth-Venkatesan <ee23d200@smail.iitm.ac.in>
2026-02-25 10:15:29 +0530

    Update term_utils.py

--- src/utils/term_utils.py
+++ src/utils/term_utils.py
@@ -1,1 +1,14 @@
+def term_to_index(term):
+    """
+    Converts term code like 'F2-2023' into sortable index.
+    """
+    sem, year = term.split("-")
+    sem_no = {"F1": 1, "F2": 2, "F3": 3}[sem]
+    return int(year) * 10 + sem_no
 
+
+def sort_terms(terms):
+    """
+    Sort term codes chronologically.
+    """
+    return sorted(terms, key=term_to_index)
