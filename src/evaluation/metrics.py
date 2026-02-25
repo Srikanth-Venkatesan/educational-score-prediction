fae71f6b3ef51fc6159a663597b3f0911be45bfa
Srikanth-Venkatesan <ee23d200@smail.iitm.ac.in>
2026-02-25 10:17:45 +0530

    Update metrics.py

--- src/evaluation/metrics.py
+++ src/evaluation/metrics.py
@@ -1,1 +1,10 @@
+import numpy as np
+from sklearn.metrics import root_mean_squared_error, mean_absolute_error
 
+
+def rmse(y_true, y_pred):
+    return np.sqrt(root_mean_squared_error(y_true, y_pred))
+
+
+def mad(y_true, y_pred):
+    return mean_absolute_error(y_true, y_pred)
