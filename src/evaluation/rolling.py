bc7ed255e6b700e76ba7374cd3227cbd61af0d26
Srikanth-Venkatesan <ee23d200@smail.iitm.ac.in>
2026-02-25 10:18:02 +0530

    Update rolling.py

--- src/evaluation/rolling.py
+++ src/evaluation/rolling.py
@@ -1,1 +1,12 @@
+def rolling_term_split(df, term_col, sorted_terms):
+    """
+    Rolling-origin evaluation generator.
+    """
+    for i in range(len(sorted_terms) - 1):
+        train_terms = sorted_terms[: i + 1]
+        test_term = sorted_terms[i + 1]
 
+        train_df = df[df[term_col].isin(train_terms)]
+        test_df = df[df[term_col] == test_term]
+
+        yield train_df, test_df, train_terms[-1], test_term
