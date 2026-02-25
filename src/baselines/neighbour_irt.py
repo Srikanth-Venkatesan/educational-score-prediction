f0df0b166dd6d5b849ee4c33d2c9bea264ecc751
Srikanth-Venkatesan <ee23d200@smail.iitm.ac.in>
2026-02-25 10:21:54 +0530

    Update neighbour_irt.py

--- src/baselines/neighbour_irt.py
+++ src/baselines/neighbour_irt.py
@@ -1,1 +1,34 @@
+from ..irt.irt_models import irt_2pl
 
+
+class NeighbourBasedIRT:
+    """
+    Expert-based IRT using precomputed mean difficulty
+    and discrimination values.
+    """
+
+    def predict(self, df):
+        df = df.copy()
+
+        df["p_correct"] = irt_2pl(
+            df["ability"],
+            df["Mean_Discrimination"],
+            df["Mean_Difficulty"]
+        )
+
+        df["y_hat"] = (df["p_correct"] >= 0.5).astype(int)
+        df["expected_score"] = df["y_hat"] * df["Mark"]
+
+        pred_scores = (
+            df.groupby("student_email")["expected_score"]
+            .sum()
+            .reset_index()
+        )
+
+        actual_scores = (
+            df.groupby("student_email")["Quiz1_score"]
+            .first()
+            .reset_index()
+        )
+
+        return pred_scores.merge(actual_scores, on="student_email")
